393a53fd209b856f65817cf29cd5087c215cc0ce
LeeMenHin <1249014784@qq.com>
2019-08-30 00:54:13 +0800

    Init kernel source code of Dreamlte

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Aug 28 15:08:28 HKT 2019"
+#define LINUX_COMPILE_BY "peter"
+#define LINUX_COMPILE_HOST "Fusion"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
